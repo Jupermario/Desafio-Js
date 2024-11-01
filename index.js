c7e592ae80072027c64eb63cc98869572dc61d48
Jupermario <mariocesar193@gmail.com>
2024-11-01 03:49:57 -0300

    Desafio Heroi

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-let nome = "mario"
-console.log("ola mundo, teste" +nome)
-console.log("bom dia")
-console.log("digite o nome do heroi:")
-//teste
+
+
+        function classificar() {
+            // Obtém os valores do input
+            const nome = document.getElementById("nomeInput").value;
+            const xp = parseInt(document.getElementById("xpInput").value);
+            let result = document.getElementById("resultado")
+            let classificacao;
+
+            // Classifica com base na quantidade de XP
+            if (xp <= 1000) {
+                classificacao = "Ferro";
+            } else if (xp >=1001 &&xp<=2000) {
+                classificacao = "Bronze";
+            } else if (xp >=2001 && xp<=5000) {
+                classificacao = "Prata";
+            } else if (xp >=5001 && xp<=7000) {
+                classificacao = "ouro";
+            }else if (xp >=7001&&  xp<=8000) {
+                    classificacao = "platina";
+            }else if (xp >=8001 && xp<=9000) {
+                classificacao = "ascendente";}
+            else if (xp >=9001 && xp<=10000) {
+                    classificacao = "Imortal";}
+            else if (xp >10001) {
+                        classificacao = "Radiante";
+
+            }         else{
+                classificacao =  "xp inválido"
+            }   document.getElementById("resultado").innerText = `${nome}, sua classificação é: ${classificacao}`
+
+        }
